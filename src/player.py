4eb93e0e5a4d21f7b4a79d67461f45d1a192cdb1
Lucas Bazemore <lucasabazemore@gmail.com>
2020-01-15 18:13:14 -0600

    create player

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player():
+    def __init__(self, location='outside'):
+        self.location = location
+
+    def move(self, location):
+        self.location = location
+
+    def where(self):
+        print(self.location)
